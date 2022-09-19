da6a6debafc44957d429fdaab03d86e578f109c9
Pascal Seitz <pascal.seitz@gmail.com>
2022-09-19 15:55:10 +0800

    add everyhing

---
+++ graph.js
@@ -0,0 +1,221 @@
+ 
+
+function tooltipPlugin({onclick, shiftX = 10, shiftY = 10, commit_hashes, commit_hash_to_msg }) {
+			let tooltipLeftOffset = 0;
+			let tooltipTopOffset = 0;
+
+			const tooltip = document.createElement("div");
+			tooltip.className = "u-tooltip";
+
+			let seriesIdx = null;
+			let dataIdx = null;
+
+			const fmtDate = uPlot.fmtDate("{M}/{D}/{YY} {h}:{mm}:{ss} {AA}");
+
+			let over;
+
+			let tooltipVisible = false;
+
+			function showTooltip() {
+				if (!tooltipVisible) {
+					tooltip.style.display = "block";
+					over.style.cursor = "pointer";
+					tooltipVisible = true;
+				}
+			}
+
+			function hideTooltip() {
+				if (tooltipVisible) {
+					tooltip.style.display = "none";
+					over.style.cursor = null;
+					tooltipVisible = false;
+				}
+			}
+
+			function setTooltip(u) {
+				showTooltip();
+
+				let top = u.valToPos(u.data[seriesIdx][dataIdx], 'y');
+				let lft = u.valToPos(u.data[        0][dataIdx], 'x');
+
+				tooltip.style.top  = (tooltipTopOffset  + top + shiftX) + "px";
+				tooltip.style.left = (tooltipLeftOffset + lft + shiftY) + "px";
+
+				//tooltip.style.borderColor = isInterpolated(dataIdx) ? interpolatedColor : seriesColors[seriesIdx - 1];
+				let pctSinceStart = (((u.data[seriesIdx][dataIdx] - u.data[seriesIdx][0]) / u.data[seriesIdx][0]) * 100).toFixed(2);
+				let commit_msg = commit_hash_to_msg[commit_hashes[dataIdx]];
+				tooltip.textContent = (
+					fmtDate(new Date(u.data[0][dataIdx] * 1e3)) + " - " + commit_msg.slice(0,170) + "\n" +
+					uPlot.fmtNum(u.data[seriesIdx][dataIdx]) + " (" + pctSinceStart + "% since start)"
+				);
+			}
+
+			return {
+				hooks: {
+					ready: [
+						u => {
+							over = u.over;
+							tooltipLeftOffset = parseFloat(over.style.left);
+							tooltipTopOffset = parseFloat(over.style.top);
+							u.root.querySelector(".u-wrap").appendChild(tooltip);
+
+							let clientX;
+							let clientY;
+
+							over.addEventListener("mousedown", e => {
+								clientX = e.clientX;
+								clientY = e.clientY;
+							});
+
+							over.addEventListener("mouseup", e => {
+								// clicked in-place
+								if (e.clientX == clientX && e.clientY == clientY) {
+									if (seriesIdx != null && dataIdx != null) {
+										onclick(u, seriesIdx, dataIdx);
+									}
+								}
+							});
+						}
+					],
+					setCursor: [
+						u => {
+							let c = u.cursor;
+
+							if (dataIdx != c.idx) {
+								dataIdx = c.idx;
+
+								if (seriesIdx != null)
+									setTooltip(u);
+							}
+						}
+					],
+					setSeries: [
+						(u, sidx) => {
+							if (seriesIdx != sidx) {
+								seriesIdx = sidx;
+
+								if (sidx == null)
+									hideTooltip();
+								else if (dataIdx != null)
+									setTooltip(u);
+							}
+						}
+					],
+					drawAxes: [
+						u => {
+							let { ctx } = u;
+
+							const interpolatedColorWithAlpha = "#fcb0f17a";
+
+							ctx.save();
+
+							ctx.strokeStyle = interpolatedColorWithAlpha;
+							ctx.beginPath();
+
+							ctx.closePath();
+							ctx.stroke();
+							ctx.restore();
+						},
+					],
+				}
+			};
+		}
+
+
+			function genPlotOpts({ title, width, height, yAxisLabel, series, alpha = 0.3, prox = 5, absoluteMode, commit_hashes, commit_hash_to_msg }) {
+            return {
+                title,
+                width,
+                height,
+                series,
+                legend: { live: false, },
+                focus: { alpha, },
+                cursor: {
+                    focus: { prox, },
+                    drag: { x: true, y: true, },
+                },
+                scales: {
+                    y: { range: (self, dataMin, dataMax) => uPlot.rangeNum(absoluteMode ? 0 : dataMin, dataMax, 0.2, true) }
+                },
+                axes: [
+                    {
+                        grid: {
+                            show: false,
+                        }
+                    },
+                    {
+                        label: yAxisLabel,
+                        space: 24,
+                        values: (self, splits) => {
+                            return splits.map(v => {
+                                return (
+                                    v >= 1e12 ? v / 1e12 + "T" :
+                                        v >= 1e9 ? v / 1e9 + "G" :
+                                            v >= 1e6 ? v / 1e6 + "M" :
+                                                v >= 1e3 ? v / 1e3 + "k" :
+                                                    v
+                                );
+                            });
+                        },
+                    },
+                ],
+								plugins: [
+                    tooltipPlugin({
+                        onclick(u, seriesIdx, dataIdx) {
+                            let thisCommit = commit_hashes[dataIdx][1];
+                            let prevCommit = (commit_hashes[dataIdx - 1] || [null, null])[1];
+                            window.open(`/compare.html?start=${prevCommit}&end=${thisCommit}&stat=${stat}`);
+                        },
+												commit_hashes,
+												commit_hash_to_msg,
+                        absoluteMode,
+                    }),
+                ],
+            };
+        }
+
+
+			function makeChart(title, data, commit_hashes, commit_hash_to_msg) {
+				console.time("chart");
+
+        let yAxisLabel = "Time in ns";
+        let series = [
+						{},
+						{ label: "NS", stroke: "red", },
+						{ label: "Variance", stroke: "blue", }
+					];
+				
+				let plotOpts = genPlotOpts({
+                        title,
+                        width: Math.floor(window.innerWidth / 3) - 16,
+                        height: 300,
+                        yAxisLabel,
+                        series,
+                        absoluteMode: true,
+												commit_hashes,
+												commit_hash_to_msg
+      	});
+
+				let uplot = new uPlot(plotOpts, data, document.body);
+
+        console.timeEnd("chart");
+			}
+
+			let wait = document.getElementById("wait");
+			wait.textContent = "Fetching data.json....";
+
+		
+      fetch("data.json").then(r => r.json()).then(name_and_data => {
+				let commit_hash_to_msg = name_and_data["commits"]
+				wait.textContent = "Rendering...";
+        for (let name in name_and_data) {
+            let data = name_and_data[name]["uplot_data"];
+            let commit_hashes = name_and_data[name]["commit_hashes"];
+            if (data && data[0].length > 2){
+				        makeChart(name, data, commit_hashes, commit_hash_to_msg);
+            }
+        }
+        wait.textContent = "";
+
+			});
+
---
+++ src/main.rs
@@ -0,0 +1,134 @@
+use chrono::NaiveDate;
+use chrono::NaiveDateTime;
+use csv::StringRecord;
+use serde::Serialize;
+use serde_json::json;
+use std::collections::HashMap;
+use std::fs;
+use std::io::Read;
+use std::io::Write;
+
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize, Serialize)]
+#[allow(dead_code)]
+struct Record {
+    ns: u64,
+    variance: u64,
+    throughput: Option<f64>,
+    commit_hash: String,
+    commit_message: String,
+    commit_date: String,
+    rustc_version: String,
+    run_date_ts: i64,
+    run_date: String,
+}
+
+impl Record {
+    // TODO FIX, incomplete date
+    fn get_commit_timestamp(&self) -> u64 {
+        let date = NaiveDate::parse_from_str(&self.commit_date, "%Y-%m-%d").unwrap();
+        let date = NaiveDateTime::new(date, Default::default());
+        date.timestamp() as u64
+    }
+}
+
+// uPlot format
+//
+// let data = [
+//  [1546300800, 1546387200],    // x-values (timestamps)  ts commit_date
+//  [        35,         71],    // y-values (series 1)    ns
+//  [        90,         15],    // y-values (series 2)    variance
+// ];
+
+fn main() {
+    let paths = fs::read_dir("./bench_results").unwrap();
+
+    let mut bench_name_to_result = HashMap::new();
+    let mut commits = HashMap::new();
+    for path in paths {
+        let path = path.unwrap();
+        let bench_test = path.file_name().to_str().unwrap().to_string();
+        let records = get_records(std::fs::File::open(path.path()).unwrap());
+        let bench_data_uplot = get_uplot_prepared_data(&records);
+        let mut bench_info = HashMap::new();
+        bench_info.insert("uplot_data", bench_data_uplot);
+        let commit_hashs = records
+            .iter()
+            .map(|record| record.commit_hash.to_string())
+            .collect::<Vec<_>>();
+        bench_info.insert(
+            "commit_hashes",
+            serde_json::to_value(&commit_hashs).unwrap(),
+        );
+        bench_name_to_result.insert(bench_test, serde_json::to_value(&bench_info).unwrap());
+
+        for record in records {
+            commits.insert(record.commit_hash, record.commit_message);
+        }
+    }
+
+    bench_name_to_result.insert(
+        "commits".to_string(),
+        serde_json::to_value(&commits).unwrap(),
+    );
+
+    let jsonstr = serde_json::to_string_pretty(&bench_name_to_result).unwrap();
+
+    std::fs::File::create("data.json")
+        .unwrap()
+        .write_all(jsonstr.as_bytes())
+        .unwrap();
+}
+
+fn get_records<R: Read>(reader: R) -> Vec<Record> {
+    let mut rdr = csv::Reader::from_reader(reader);
+    let mut records = vec![];
+
+    for result in rdr.records() {
+        let record: StringRecord = result.unwrap();
+        let record: Record = record.deserialize(None).unwrap();
+
+        records.push(record);
+    }
+    records
+}
+
+fn get_uplot_prepared_data(records: &[Record]) -> serde_json::Value {
+    let mut timestamps = vec![];
+    let mut duration = vec![];
+    let mut variance = vec![];
+
+    for record in records {
+        let ts = record.get_commit_timestamp();
+        timestamps.push(ts);
+        duration.push(record.ns);
+        variance.push(record.variance);
+    }
+    json!(vec![timestamps, duration, variance])
+}
+
+//fn main() {
+//let mut rdr = csv::Reader::from_reader(io::stdin());
+
+//let mut timestamps = vec![];
+//let mut duration = vec![];
+//let mut variance = vec![];
+
+//for result in rdr.records() {
+//let record: StringRecord = result.unwrap();
+//let record: Record = record.deserialize(None).unwrap();
+
+//let ts = record.get_commit_timestamp();
+//timestamps.push(ts);
+//duration.push(record.ns);
+//variance.push(record.variance);
+//}
+//let json = json!(vec![timestamps, duration, variance]);
+//let jsonstr = serde_json::to_string(&json).unwrap();
+//std::fs::File::create("data2.json")
+//.unwrap()
+//.write_all(jsonstr.as_bytes())
+//.unwrap();
+////println!("{}", jsonstr);
+//}
