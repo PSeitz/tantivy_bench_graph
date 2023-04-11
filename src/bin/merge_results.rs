0d06e810b4b01514cb0527d83f94478a426ed085
Pascal Seitz <pascal.seitz@gmail.com>
2023-04-11 12:59:42 +0800

    print when merging files

--- src/bin/merge_results.rs
+++ src/bin/merge_results.rs
@@ -2,12 +2,13 @@ use std::{
     collections::HashMap,
     fs::{self, OpenOptions},
     io::Write,
+    path::PathBuf,
 };
 
 fn main() {
     let entries = fs::read_dir("./bench_results").unwrap();
 
-    let mut bench_name_to_path: HashMap<String, Vec<_>> = HashMap::new();
+    let mut bench_name_to_paths: HashMap<String, Vec<_>> = HashMap::new();
     for entry in entries {
         if let Ok(entry) = entry {
             let file_name = entry.file_name().to_str().unwrap().to_string();
@@ -15,35 +16,47 @@ fn main() {
                 .rsplit_once("::")
                 .map(|(_suffix, bench_name)| bench_name.to_string())
                 .unwrap_or(file_name.to_string());
-            let bench_paths = bench_name_to_path
+            let bench_paths = bench_name_to_paths
                 .entry(bench_name.to_string())
                 .or_default();
             bench_paths.push(entry.path());
         }
     }
     // get duplicates
-    let iter = bench_name_to_path
+    let iter_duplicates = bench_name_to_paths
         .iter()
         .filter(|(_name, paths)| paths.len() > 1);
 
-    for (_name, paths) in iter {
-        // newest file is the target
-        let target = paths
+    for (_name, paths) in iter_duplicates {
+        let last_modified: Vec<(PathBuf, std::time::SystemTime)> = paths
             .iter()
-            .max_by_key(|path| {
-                fs::metadata(path)
-                    .unwrap()
-                    .modified()
-                    .expect("could not get last modified of file")
+            .map(|path| {
+                (
+                    path.to_owned(),
+                    fs::metadata(path)
+                        .unwrap()
+                        .modified()
+                        .expect("could not get last modified of file"),
+                )
             })
+            .collect::<Vec<_>>();
+        // newest file is the target
+        let target = last_modified
+            .iter()
+            .max_by_key(|(_path, last_modified)| last_modified)
+            .map(|(path, _last_modified)| path)
             .unwrap();
 
-        let other_paths = paths.iter().filter(|path| path != &target);
+        let other_paths = paths
+            .iter()
+            .filter(|path| path != &target)
+            .collect::<Vec<_>>();
+        println!("Merging {:?} into {:?}", other_paths, target);
 
         let mut file = OpenOptions::new()
             .write(true)
             .append(true)
-            .open(target)
+            .open(&target)
             .unwrap();
 
         for duplicate in other_paths {
